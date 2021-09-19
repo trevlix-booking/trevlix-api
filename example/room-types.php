e0621afd677d974bd142c0cbbec6c29c9cf52513
trevlix-booking <83665403+trevlix-booking@users.noreply.github.com>
2021-09-19 17:12:15 +0200

    Add files via upload

---
+++ example/room-types.php
@@ -0,0 +1,25 @@
+<?php
+$path = '../';
+require $path.'init.php';
+include $path.'template/head.php';
+
+echo "<h1>GET room-types</h1>";
+
+$response = \Httpful\Request::get($api_json_url.'room-types/')
+->authenticateWith($api_key_username, $api_key_password)
+->expectsJson()
+->send();
+
+if ($response->code != 200) {
+    die('Error '.$response->code.': '.$response->body->error);
+}
+
+$roomTypes = $response->body->roomTypes;
+
+echo "<h2>roomTypes</h2>";
+
+echo "<pre>";
+print_r($roomTypes);
+echo "</pre>";
+
+include $path.'template/back.php';
---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+$path = './';
+include $path.'template/head.php';
+?>
+  
+  <h1>Trevlix API demo</h1>
+	<p>API DOC: <a href="https://app.swaggerhub.com/apis-docs/trevlix/trevlix/0.3.0" target="_blank">https://app.swaggerhub.com/apis-docs/trevlix/trevlix/0.3.0</a></p>
+
+  <p>Please enter your username and password in the config.php file before testing the API.</p>
+
+  <h3>Tests</h3>
+  <ul>
+    <li><a href="example/ping.php">GET ping</a>
+  </ul>
+
+  <h3>Property</h3>
+  <ul>
+    <li><a href="example/property.php">GET property</a>
+    <li><a href="example/room-types.php">GET room-types</a>
+    <li><a href="example/account.php">GET account</a>
+
+  </ul>
+
+  <h3>Availability</h3>
+  <ul>
+    <li><a href="example/availability-all.php">GET availability-all</a>
+    <li><a href="example/availability.php">GET availability</a>
+  </ul>
+
+  <h3>Bookings</h3>
+  <ul>
+    <li><a href="example/bookings.php">GET bookings</a>
+    <li><a href="example/bookings-latest.php">GET bookings-latest</a>
+    <li><a href="example/booking-create.php">POST booking-create</a>
+    <li><a href="example/booking-cancel.php">POST booking-cancel</a>
+  </ul>
+
+</body>
+</html>
